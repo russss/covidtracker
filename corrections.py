dd1e657477364239a488e82bfa6a54f144478285
Russ Garrett <russ@garrett.co.uk>
2020-08-08 11:19:41 +0100

    Ignore non-England LTLAs

--- corrections.py
+++ corrections.py
@@ -2,31 +2,6 @@ import numpy as np
 import coviddata.uk
 from datetime import date, timedelta
 
-WALES_LTLAS = [
-"Isle of Anglesey",
-"Gwynedd",
-"Conwy",
-"Denbighshire",
-"Flintshire",
-"Wrexham",
-"Ceredigion",
-"Pembrokeshire",
-"Carmarthenshire",
-"Swansea",
-"Neath Port Talbot",
-"Bridgend",
-"Vale of Glamorgan",
-"Cardiff",
-"Rhondda Cynon Taf",
-"Caerphilly",
-"Blaenau Gwent",
-"Torfaen",
-"Monmouthshire",
-"Newport",
-"Powys",
-"Merthyr Tydfil"
-]
-
 
 def correct_scottish_data(scot_data):
     """ Scotland started adding the UK "pillar 2" tests to their figures on
@@ -56,14 +31,14 @@ def cases_by_nhs_region(la_region_mapping):
     nhs_regions = []
     for a in regions["location"]:
         name = str(a.data)
-        if name in WALES_LTLAS:
-            nhs_regions.append("Wales")
-            continue
         if name == "Cornwall and Isles of Scilly":
             name = "Cornwall"
         if name == "Hackney and City of London":
             name = "Hackney"
-        nhs_regions.append(la_region_mapping["nhs_name"][name])
+        try:
+            nhs_regions.append(la_region_mapping["nhs_name"][name])
+        except KeyError:
+            nhs_regions.append('Not England')
 
     res = regions.assign_coords({"location": nhs_regions})
-    return res.groupby("location").sum().drop_sel(location='Wales')
+    return res.groupby("location").sum().drop_sel(location='Not England')
